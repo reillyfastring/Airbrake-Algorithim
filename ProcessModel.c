5111c1548ef8c94d061a934ac7243fec8c898e19
Fastring,Reilly <68809313+reillyfastring@users.noreply.github.com>
2025-02-25 17:11:24 -0500

    Update ProcessModel.c

--- ProcessModel.c
+++ ProcessModel.c
@@ -4,12 +4,12 @@
 #define GyroC 0.001
 #define GPSC 0.05
 
-void processModel(double* A, int n, double* t, double* DeployAngle, double* sensorVector) {
+void processModel(double sigma, int n, double t, double* DeployAngle, double* sensorVector) {
     //drag first(1)-> velocity(1) -> position(1) -> angle(3) -> bias(3)
     for (int i = 1; i < (n+1); i++) {
         double magA = sqrt(pow(sensorVector[1], 2) + pow(sensorVector[2], 2) + pow(sensorVector[3], 2));
         //Prediction for dragZ
-        sigma[i*SIZE + 1] += drag(theta, sigma[i*SIZE + 2]);
+        sigma[i*SIZE + 1] += drag(DeployAngle, sigma[i*SIZE + 2]);
         //prediction for velocityZ
         sigma[i*SIZE + 2] += sin(magA - sigma[i*SIZE + 1])*t;
         //prediction for PositionZ
@@ -23,7 +23,7 @@ void processModel(double* A, int n, double* t, double* DeployAngle, double* sens
         //prediction for Accelerometer Bias
         sigma[i*SIZE + 7] += AccC*randN();
         //prediction for Gyroscope Bias
-        sigma[i*SIZE + 8] += GryoC*randN();
+        sigma[i*SIZE + 8] += GyroC*randN();
         //prediction for GPS bias
         sigma[i*SIZE + 9] += GPSC*randN();
     }
